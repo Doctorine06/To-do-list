44f9ef1692fbf6a5ec3dea2d983de4bbc6754e8e
Think Out Loud KC <fergtheartist@gmail.com>
2023-03-21 16:46:38 -0400

    no src

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+change in Node.js
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
